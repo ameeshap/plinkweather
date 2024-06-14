f538154fc7c71ab4452089fb2b5600edc8a14106
SageCT <sage.turner@publicissapient.com>
2024-06-14 15:29:32 -0500

    Added colors for real

--- weatherapp/src/components/Navbar.jsx
+++ weatherapp/src/components/Navbar.jsx
@@ -5,18 +5,25 @@ import { IoHomeOutline } from 'react-icons/io5'
 
 const Navbar = () => {
   return (
-    <main>
+    <main className="flex flex-row justify-between w-screen h-24">
+      <p className="text-lg text-tempblue">GOOD MORNING GAMERS!!!</p>
       <ul>
         {/* Link to Current Location */}
-        <li className="">
-          <IoHomeOutline color="black" />
+        <li className="w-10 h-10">
+          <IoHomeOutline color="black" size={45} />
         </li>
 
         {/* Link to list of locations */}
-        <li className=""></li>
+        <li className="">
+          <IoHomeOutline color="black" size={45}>
+            <Link href="/"></Link>
+          </IoHomeOutline>
+        </li>
 
         {/* Link to Map of current location */}
-        <li className=""></li>
+        <li className="">
+          <IoHomeOutline color="black" size={45} />
+        </li>
       </ul>
     </main>
   )
--- weatherapp/tailwind.config.js
+++ weatherapp/tailwind.config.js
@@ -1,44 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{js,jsx}',
     './components/**/*.{js,jsx}',
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
-      colors : {
-        'bgwhite': '#F5F5F5',
-        'cardgray': '#C3D98F',
-        'tempblue' : '#85BDBF',
-        'tempred' : '#EC9798',
-        'tempgreen' : '#C3D98F'
+      fontFamily: {
+        inter: ['Inter'],
+      },
+      colors: {
+        bgwhite: '#F5F5F5',
+        cardgray: '#C3D98F',
+        tempblue: '#85BDBF',
+        tempred: '#EC9798',
+        tempgreen: '#C3D98F',
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require('daisyui')],
+  plugins: [require('tailwindcss-animate'), require('daisyui')],
 }
