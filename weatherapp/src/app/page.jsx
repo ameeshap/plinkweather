4d54d746b62a929562b0901b0159abe2ed76d55e
SageCT <sage.turner@publicissapient.com>
2024-06-14 12:58:42 -0500

    Removed pre-made files

--- weatherapp/src/app/page.jsx
+++ weatherapp/src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-between bg-bgwhite">
-      
+      {/* Content for Page */}
     </main>
   )
 }
